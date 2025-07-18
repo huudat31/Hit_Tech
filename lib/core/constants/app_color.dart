d741a3449e5e0917f64947214e0f4fb088d23849
huudat31 <phanhuudat310708@gmail.com>
2025-07-18 16:26:27 +0700

    update colors for app

--- lib/core/constants/app_color.dart
+++ lib/core/constants/app_color.dart
@@ -1,7 +1,38 @@
 import "package:flutter/material.dart";
 
 class AppColors {
-  static const Color primaryAppColor = Color(0xFF2454F8);
-  static const Color borderColors = Color(0xFF989DA1);
-  static const Color backgroundColor = Color.fromARGB(255, 255, 255, 255);
+  // Blue
+  static const Color bLight = Color(0xFFe8f4fc);
+  static const Color bLightHover = Color(0xFFdceefb);
+  static const Color bLightActive = Color(0xFFb6dbf6);
+  static const Color bNormal = Color(0xFF148ce2);
+  static const Color bNormalHover = Color(0xFF127ecb);
+  static const Color bNormalActive = Color(0xFF1070b5);
+  static const Color bDark = Color(0xFF0f69aa);
+  static const Color bDarkHover = Color(0xFF0c5488);
+  static const Color bDarkActive = Color(0xFF093f66);
+  static const Color bDarker = Color(0xFF07314f);
+
+  //Black
+  static const Color lighter = Color(0xFF8c8c8c);
+  static const Color lightHover = Color(0xFF757575);
+  static const Color lightActive = Color(0xFF595959);
+  static const Color normal = Color(0xFF000000);
+  static const Color normalHover = Color(0xFF000000);
+  static const Color normalActive = Color(0xFF000000);
+  static const Color dark = Color(0xFF000000);
+  static const Color darkHover = Color(0xFF000000);
+  static const Color darkActive = Color(0xFF000000);
+
+  //White
+  static const Color wWhite = Color(0xFFffffff);
+  static const Color wWhiteHover = Color(0xFFfcfdfd);
+  static const Color wWhiteActive = Color(0xFFf5f7f9);
+  static const Color wLight = Color(0xFFfcfdfd);
+  static const Color wLightHover = Color(0xFFf5f7f9);
+  static const Color wLightActive = Color(0xFFf0f3f6);
+  static const Color wNormal = Color(0xFFf9fafb);
+  static const Color wNormalHover = Color(0xFFf2f4f7);
+  static const Color wNormalActive = Color(0xFFebeef2);
+  static const Color wDark = Color(0xFFf6f8f9);
 }
