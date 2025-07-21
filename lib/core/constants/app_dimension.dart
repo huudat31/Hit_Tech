bb6e8251996c8dc61dec0408fa22871e5bc84785
huudat31 <phanhuudat310708@gmail.com>
2025-07-21 17:05:03 +0700

    update color demension message and weight_selection UI

--- lib/core/constants/app_dimension.dart
+++ lib/core/constants/app_dimension.dart
@@ -10,9 +10,51 @@ class AppDimensions {
 
   // Spacing
   static const double spaceXS = 8.0;
+  static const double spaceXXS = 10.0;
+  static const double spaceXXXS = 12.0;
+  static const double spaceXXSPlus = 14.0;
+  static const double spaceX = 15.0;
   static const double spaceS = 16.0;
+  static const double spaceSM = 18.0;
   static const double spaceM = 19.0;
-  static const double spaceL = 24.0;
-  static const double spaceXL = 35.0;
-  static const double spaceXXL = 45.0;
+  static const double spaceML = 20.0;
+  static const double spaceL = 22.0;
+  static const double space24 = 24.0;
+  static const double spaceXL = 30.0;
+  static const double spaceXXL = 32.0;
+  static const double spaceXXXL = 40.0;
+  static const double spaceXXXLarge = 50.0;
+  static const double spaceXXXLargePlus = 55.0;
+  // Sizes
+  static const double iconSize = 24.0;
+  static const double iconSizeSmall = 20.0;
+  static const double iconSizeLarge = 30.0;
+  static const double iconSizeExtraLarge = 40.0;
+  static const double iconSizeXXL = 50.0;
+  static const double iconSizeXXXL = 60.0;
+  static const double iconSizeXXXLarge = 70.0;
+  static const double iconSizeXXXLargePlus = 80.0;
+  static const double textSizeXXS = 10.0;
+  static const double textSizeXS = 12.0;
+  static const double textSizeS = 14.0;
+  static const double textSizeM = 16.0;
+  static const double textSizeML = 18.0;
+  static const double textSizeL = 20.0;
+  static const double textSizeXL = 24.0;
+  static const double textSizeXXL = 28.0;
+  static const double textSizeXXXL = 32.0;
+  static const double textSizeXXXLarge = 36.0;
+
+  //circular progress indicator
+  static const double circularXS = 12.0;
+  static const double circularS = 20.0;
+
+  static const double circularM = 25.0;
+  static const double circularL = 40.0;
+  static const double circularXL = 50.0;
+  static const double circularXXL = 60.0;
+
+  // height and width
+  static const double heightButton = 50.0;
+  static const double normal = double.infinity;
 }
